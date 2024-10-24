8bcef7246338b642f953b2cfa5d9c24466566ece
Struan Brown <struan@tonnard.co.uk>
2024-10-24 18:06:16 +0100

    Commit chnages

---
+++ portfolio/main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Project, Tag
+
+def home(request):
+    return render(request, "home.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def project(request, id):
+    return render(request, "project.html")
